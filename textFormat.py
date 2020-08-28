7374b87b2c1cc61c3dd92985cc38a6beb26c6b9e
Xander Gennarelli <xander.gennarelli@gmail.com>
2020-08-28 16:34:52 -0500

    tab fix

--- textFormat.py
+++ textFormat.py
@@ -1,13 +1,12 @@
 def format(fNameIn, fNameOut):
-    with open(fNameIn) as f:
-        l = f.readline()
-        while l != '':
-            o = open(fNameOut, 'a')
-            str = "  cout << \"" + l[0:65] + "\"\n       << \"" + l[65:130] + "\"\n       << \"" + l[130:len(l)-1] + "\" << endl;\n"
-            o.write(str)
-            o.close()
-            l = f.readline()
-
+  with open(fNameIn) as f:
+    l = f.readline()
+      while l != '':
+        o = open(fNameOut, 'a')
+          str = "  cout << \"" + l[0:65] + "\"\n       << \"" + l[65:130] + "\"\n       << \"" + l[130:len(l)-1] + "\" << endl;\n"
+          o.write(str)
+          o.close()
+          l = f.readline()
 
 fIn = "/home/xander/Documents/python/inFile.txt"
 fOut = "/home/xander/Documents/python/outFile.txt"
